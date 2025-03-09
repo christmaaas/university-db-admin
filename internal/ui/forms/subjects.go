6db9c4f39c603249f37aa8505bc2d83a4dcc504b
Christmas <slinko.art@mail.ru>
2025-03-09 19:14:51 +0300

    implemented ui forms for subjects

---
+++ internal/ui/forms/subjects.go
@@ -0,0 +1,205 @@
+package forms
+
+import (
+	"context"
+	"fmt"
+	"university-db-admin/internal/domain"
+	"university-db-admin/internal/repository"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func ShowSubjectsForm(content *fyne.Container, action string, r *repository.Repository) {
+	content.Objects = nil
+
+	switch action {
+	case "Добавить":
+		showAddSubjectsForm(content, r)
+	case "Удалить":
+		showDeleteSubjectsForm(content, r)
+	case "Обновить":
+		showUpdateSubjectsForm(content, r)
+	case "Просмотреть":
+		showSubjectsList(content, r)
+	}
+
+	content.Refresh()
+}
+
+func showAddSubjectsForm(content *fyne.Container, r *repository.Repository) {
+	nameEntry := widget.NewEntry()
+	nameEntry.SetPlaceHolder("Название")
+
+	dscrEntry := widget.NewEntry()
+	dscrEntry.SetPlaceHolder("Описание")
+
+	submitButton := widget.NewButton("Добавить", func() {
+		sbj := domain.Subject{
+			Name:        nameEntry.Text,
+			Description: dscrEntry.Text,
+		}
+
+		err := r.Subjects.Create(context.Background(), sbj)
+		showResult(content, err, "Предмет добавлен")
+	})
+
+	form := container.NewVBox(
+		widget.NewLabel("Добавление предмета"),
+		nameEntry,
+		dscrEntry,
+		submitButton,
+	)
+
+	content.Add(form)
+}
+
+func showDeleteSubjectsForm(content *fyne.Container, r *repository.Repository) {
+	idEntry := widget.NewEntry()
+	idEntry.SetPlaceHolder("ID предмета")
+
+	deleteButton := widget.NewButton("Удалить", func() {
+		err := r.Subjects.Delete(context.Background(), parseUint64(idEntry.Text))
+		showResult(content, err, "Предмет удален")
+	})
+
+	form := container.NewVBox(
+		widget.NewLabel("Удаление предмета"),
+		idEntry,
+		deleteButton,
+	)
+
+	content.Add(form)
+}
+
+func showUpdateSubjectsForm(content *fyne.Container, r *repository.Repository) {
+	idEntry := widget.NewEntry()
+	idEntry.SetPlaceHolder("ID предмета")
+
+	nameEntry := widget.NewEntry()
+	nameEntry.SetPlaceHolder("Новое название")
+
+	dscrEntry := widget.NewEntry()
+	dscrEntry.SetPlaceHolder("Новое описание")
+
+	updateButton := widget.NewButton("Обновить", func() {
+		sbj := domain.Subject{
+			ID:          parseUint64(idEntry.Text),
+			Name:        nameEntry.Text,
+			Description: dscrEntry.Text,
+		}
+
+		err := r.Subjects.Update(context.Background(), sbj.ID, sbj)
+		showResult(content, err, "Предмет обновлен")
+	})
+
+	form := container.NewVBox(
+		widget.NewLabel("Обновление предмета"),
+		idEntry,
+		nameEntry,
+		dscrEntry,
+		updateButton,
+	)
+
+	content.Add(form)
+}
+
+func showSubjectsList(content *fyne.Container, r *repository.Repository) {
+	content.Objects = nil
+
+	headers := []string{
+		"ID предмета",
+		"Название",
+		"Описание",
+	}
+	var data [][]string
+
+	filterEntry := widget.NewEntry()
+	filterEntry.SetPlaceHolder("Введите значение")
+
+	filterOptions := map[string]uint8{
+		"Все":      0,
+		"ID":       1,
+		"Название": 2,
+	}
+
+	options := []string{
+		"Все",
+		"ID",
+		"Название",
+	}
+	var selectedField uint8
+	filterSelect := widget.NewSelect(options, func(value string) {
+		selectedField = filterOptions[value]
+
+		if selectedField == 0 {
+			filterEntry.SetText("")
+			filterEntry.Disable()
+		} else {
+			filterEntry.Enable()
+		}
+	})
+
+	applyFilterButton := widget.NewButton("Применить фильтр", func() {
+		data = nil
+
+		var (
+			subjects []domain.Subject
+			subject  domain.Subject
+			err      error
+		)
+
+		switch selectedField {
+		case 0:
+			subjects, err = r.Subjects.FindAll(context.Background())
+		case 1:
+			subject, err = r.Subjects.FindOne(context.Background(), parseUint64(filterEntry.Text))
+			if err == nil {
+				subjects = append(subjects, subject)
+			}
+		case 2:
+			subject, err = r.Subjects.FindByName(context.Background(), filterEntry.Text)
+			if err == nil {
+				subjects = append(subjects, subject)
+			}
+		}
+
+		if err != nil {
+			showResult(content, err, "Ошибка при поиске")
+			return
+		}
+
+		for _, s := range subjects {
+			data = append(data, []string{
+				fmt.Sprintf("%d", s.ID),
+				s.Name,
+				s.Description,
+			})
+		}
+
+		content.Objects = content.Objects[:1] // Only filter widgets remain
+		content.Add(updateTable(headers, data))
+		content.Refresh()
+	})
+
+	subjects, _ := r.Subjects.FindAll(context.Background())
+	for _, s := range subjects {
+		data = append(data, []string{
+			fmt.Sprintf("%d", s.ID),
+			s.Name,
+			s.Description,
+		})
+	}
+
+	filterContainer := container.NewVBox(
+		widget.NewLabel("Фильтрация предметов"),
+		filterSelect,
+		filterEntry,
+		applyFilterButton,
+	)
+
+	content.Add(filterContainer)
+	content.Add(updateTable(headers, data))
+	content.Refresh()
+}
